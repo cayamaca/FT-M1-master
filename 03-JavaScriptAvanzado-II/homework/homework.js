44931137f8faa200c5ed4541a88c0973683a73ee
CARMENMALAGON <cymalagon@misena.edu.co>
2022-05-25 15:32:54 -0500

    Homework javaScript II

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -3,10 +3,13 @@
 // Closures
 
 function counter() {
-  /*
+
+ /*
   Ejercicio 1
 
-  La función counter debe retornar otra función. Esta función retornada debe actuar como un contador, retornando un valor numérico que empieza en 1 e incrementa con cada invocación.
+ // La función counter debe retornar otra función. 
+ //Esta función retornada debe actuar como un contador, 
+ //retornando un valor numérico que empieza en 1 e incrementa con cada invocación.
 
   Ejemplo:
   const nuevoContador = counter()
@@ -19,15 +22,31 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+  var nuevoContador = 1;
+  return function(){
+   return nuevoContador++;
+  }
+
 }
+
+ 
 
 function cacheFunction(cb) {
   /*
   Ejercicio 2
 
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
-
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+  //Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché
+   para el callback que recibe por parámetro (cb); 
+  //es decir, que "recuerde" el resultado de cada operación que hace, de manera que, 
+  al realizar una operación por segunda vez, 
+  se pueda obtener el resultado de esa "memoria" sin tener que 
+  efectuar otra vez cálculos que ya se hicieron anteriormente.
+  //cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg)
+   e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado
+  de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, 
+  y su valor el resultado de la correspondiente invocación a cb) de manera que, 
+  la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, 
+  porque el resultado estará guardado en la "memoria caché".
 
 
   Ejemplo:
@@ -41,9 +60,22 @@ function cacheFunction(cb) {
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
+ var cache = {};
+ return function(arg){
+   if(cache.hasOwnProperty(arg)){
+  return cache[arg];
+   }else {
+     cache[arg] = cb(arg)
+   }
+}
+  
+}
+function multipicacion(n){
+  return n * n;
 }
 
 // Bind
+
 
 var instructor = {
   nombre: "Franco",
