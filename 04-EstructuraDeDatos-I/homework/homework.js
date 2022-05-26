306aaabd01a7aec8b5d35ebba4f15f520109c137
CARMENMALAGON <cymalagon@misena.edu.co>
2022-05-26 15:30:22 -0500

    homework estructura de datos parte I

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -21,8 +21,13 @@ function nFactorial(n) {
 }
 
 function nFibonacci(n) {
-
-    
+  if(n===0){
+    return 0;
+  }
+  if(n === 1){
+    return 1;
+  }
+  return nFibonacci(n-1) + nFibonacci(n-2);
 
 }
 
