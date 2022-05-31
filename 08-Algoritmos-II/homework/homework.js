e38d00d83fa0e7425a5a3df709efbaa3b9a71af2
CARMENMALAGON <cymalagon@misena.edu.co>
2022-05-31 17:52:41 -0500

    homework/Algoritmos II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,19 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  let quickSort = (array);
+    let arr = array.length;
+    for (var i = 0; i < arr; i++) {
+        for (let j = 0; j < arr; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+
 }
 
 function mergeSort(array) {
@@ -14,6 +27,19 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let mergeSort = (array);
+    let arr = array.length;
+    for (let i = 0; i < arr; i++) {
+        for (let j = 0; j < arr; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+
 
 }
 
