374a07179bb6118067ced71360b11407c9a00985
CARMENMALAGON <cymalagon@misena.edu.co>
2022-05-31 14:01:24 -0500

    homework/Algoritmos-I

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,6 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let arr = [1];
+  var dividiendo = 2;
+  while (num !== 1){
+    if(num % dividiendo === 0){
+      arr.push(dividiendo);
+      num = num  / dividiendo;
+
+    }else{
+      dividiendo++;
+    }
+
+  }
+  return arr;
 
 }
 
@@ -14,7 +27,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let bubbleSort = (array);
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+  
 }
 
 
@@ -23,6 +48,19 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+var insertionSort = (array);
+    var arr = array.length;
+    for (var i = 0; i < arr; i++) {
+        for (let j = 0; j < arr; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+
 
 }
 
@@ -32,6 +70,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  let selectionSort = (array);
+    var arr = array.length;
+    for (let i = 0; i < arr; i++) {
+        for (let j = 0; j < arr; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
 
 }
 
